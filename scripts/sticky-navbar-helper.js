fd174caee85f7186ab4dfd4c8136f6706aaabd6f
Kieran <k.powell@1xinternet.de>
2024-08-28 17:11:32 +0200

    More SCSS Changes

--- scripts/sticky-navbar-helper.js
+++ scripts/sticky-navbar-helper.js
@@ -4,8 +4,6 @@ navbar.style.setProperty("--nav-bar-height", navbar.offsetHeight + 2 + "px");
 let lastScroll = window.scrollY;
 const topOffset = navbar.offsetHeight;
 
-console.log(topOffset);
-
 stickyNavbarHelper = () => {
   if (window.scrollY >= topOffset && window.scrollY < lastScroll) {
     navbar.classList.add("header-bar--scroll-visible");
@@ -15,4 +13,5 @@ stickyNavbarHelper = () => {
 
   lastScroll = window.scrollY;
 };
+
 window.addEventListener("scroll", stickyNavbarHelper);
